d66eb5ea2778f2932198cd780d136781edf6f36c
Mohammadreza Moghimi <mimr.moghimi@gmail.com>
2020-03-20 01:22:15 +0330

    create gulp task for nojs theme and making another version of ther for bayan blogs who has no js enabled

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,17 @@ gulp.task('compass', function() {
     }));
 });
 
+gulp.task('compass-nojs', function() {
+  return gulp.src('src/main-nojs.scss')
+    .pipe(compass({
+      sass: 'src',
+      image: 'src/img',
+      css: 'dist',
+      sourcemap: true,
+      style: 'compressed'
+    }));
+});
+
 gulp.task('default', function() {
   gulp.watch('src/css/**/*.scss', gulp.series('compass'));
 });
